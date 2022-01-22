53694fbdb04fb21ac63e42ccf904bd9d09974590
Francella Aguilar <francellaaguilar@Francellas-MBP.attlocal.net>
2022-01-22 10:15:30 -0500

    added new js files and mofidied html, css

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+var startButton = document.getElementById('start-btn');
+
+startButton.addEventListener('click', startQuiz);
+
+function startQuiz() {
+    console.log('Started')
+}
+
+function setNextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
